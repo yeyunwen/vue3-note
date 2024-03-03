17d642b85357132d81ee227f3bcd0889e764f8e0
yeyunwen <1571374338@qq.com>
2024-03-03 22:45:46 +0800

    feat: 修改🪣数据结构

--- 01.框架设计概况/code/04.js
+++ 01.框架设计概况/code/04.js
@@ -1,9 +1,4 @@
-const bucket = new Set();
-
-const data = {
-  text: "hello world",
-};
-
+const bucket = new WeakMap(); // 之所以用WeakMap，是因为WeakMap是弱引用。 而bucket中的的key是要代理的目标，如果目标不被用户使用，也就没有收集依赖的必要。
 let activeEffect = null;
 
 const effect = (fn) => {
@@ -11,16 +6,40 @@ const effect = (fn) => {
   fn();
 };
 
+const track = (target, key) => {
+  if (!activeEffect) return;
+  let depsMap = bucket.get(target);
+  if (!depsMap) {
+    bucket.set(target, (depsMap = new Map()));
+  }
+  let deps = depsMap.get(key);
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()));
+  }
+  deps.add(activeEffect);
+};
+
+const trigger = (target, key) => {
+  const depsMap = bucket.get(target);
+  if (!depsMap) return;
+  const deps = depsMap.get(key);
+  deps && deps.forEach((fn) => fn());
+};
+
+const data = {
+  text: "hello world",
+};
+
 const obj = new Proxy(data, {
   get(target, key) {
-    if (activeEffect) {
-      bucket.add(activeEffect);
-    }
+    track(target, key);
+
     return target[key];
   },
   set(target, key, newValue) {
     target[key] = newValue;
-    bucket.forEach((fn) => fn());
+    trigger(target, key);
+
     return true;
   },
 });
