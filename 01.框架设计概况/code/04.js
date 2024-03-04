74f24608a90b7cc2ad839bd6168089d1463af368
yeyunwen <1571374338@qq.com>
2024-03-04 16:09:05 +0800

    feat: 分支切换与cleanup

--- 01.框架设计概况/code/04.js
+++ 01.框架设计概况/code/04.js
@@ -1,9 +1,22 @@
 const bucket = new WeakMap(); // 之所以用WeakMap，是因为WeakMap是弱引用。 而bucket中的的key是要代理的目标，如果目标不被用户使用，也就没有收集依赖的必要。
 let activeEffect = null;
 
+const cleanup = (effectFn) => {
+  for (const deps of effectFn.deps) {
+    deps.delete(effectFn);
+  }
+  effectFn.deps.length = 0;
+};
+
 const effect = (fn) => {
-  activeEffect = fn;
-  fn();
+  const effectFn = () => {
+    cleanup(effectFn);
+    activeEffect = effectFn;
+    fn();
+  };
+  effectFn.deps = [];
+
+  effectFn();
 };
 
 const track = (target, key) => {
@@ -17,17 +30,21 @@ const track = (target, key) => {
     depsMap.set(key, (deps = new Set()));
   }
   deps.add(activeEffect);
+
+  activeEffect.deps.push(deps);
 };
 
 const trigger = (target, key) => {
   const depsMap = bucket.get(target);
   if (!depsMap) return;
   const deps = depsMap.get(key);
-  deps && deps.forEach((fn) => fn());
+  const depsToRun = new Set(deps);
+  depsToRun.forEach((fn) => fn());
 };
 
 const data = {
   text: "hello world",
+  ok: true,
 };
 
 const obj = new Proxy(data, {
@@ -45,9 +62,5 @@ const obj = new Proxy(data, {
 });
 
 effect(() => {
-  document.body.innerHTML = obj.text;
+  document.body.innerHTML = obj.ok ? obj.text : "not";
 });
-
-setTimeout(() => {
-  obj.text = "hello vue";
-}, 1000);
